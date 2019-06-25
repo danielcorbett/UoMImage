0739ce919083ad925d200280bd07cdce2d3f55b4
Daniel Corbett <50834628+danielcorbett@users.noreply.github.com>
2019-06-25 10:24:09 +0100

    Add files via upload

---
+++ scripts/virtualbox.sh
@@ -0,0 +1,13 @@
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+
+# required for VirtualBox 4.3.26
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install gcc kernel-devel kernel-headers dkms make bzip2 perl
+KERN_DIR=/usr/src/kernels/`uname -r`
+export KERN_DIR
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+
