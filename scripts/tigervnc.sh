50d893325e268d15fc42ae1581458b09d43e2898
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-11 15:06:01 +0100

    Updated tigervnc.sh v3

--- scripts/tigervnc.sh
+++ scripts/tigervnc.sh
@@ -6,9 +6,9 @@ VNCSERVICENUM=vncserver\@:0.service
 yum install tigervnc-server -y
 cp /lib/systemd/system/$VNCSERVICE /etc/systemd/system/$VNCSERVICE
 sed -i 's/<USER>/vagrant/' /etc/systemd/system/$VNCSERVICE
-echo "<‰ôFmÂ¦z" > ~vagrant/.vnc/passwd
-chown -R vagrant ~vagrant/.vnc
-chmod 600 ~vagrant/.vnc/passwd
+echo "<‰ôFmÂ¦z" > /home/vagrant/.vnc/passwd
+chown -R vagrant /home/vagrant/.vnc
+chmod 600 /home/vagrant/.vnc/passwd
 systemctl daemon-reload
 systemctl enable $VNCSERVICENUM
 systemctl start $VNCSERVICENUM
