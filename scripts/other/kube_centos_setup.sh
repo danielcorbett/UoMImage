c6f6747aba9946e8cc7f881015cec0e6d3817754
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-16 14:48:50 +0100

    Updated kube script

--- scripts/other/kube_centos_setup.sh
+++ scripts/other/kube_centos_setup.sh
@@ -13,6 +13,7 @@ cat >> /etc/yum.repos.d/kubernetes.repo <<EOF
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
+gpgcheck=0
 EOF
 
 yum remove kubernetes kubernetes-master kubernetes-node kubernetes-client -y
@@ -29,12 +30,18 @@ swapoff -a
 kubeadm reset -f
 kubeadm init
 
-cp /etc/kubernetes/admin.conf ~/.kube/config
+sleep 10
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+sleep 10
+
+cp /etc/kubernetes/admin.conf ~$USERNAME/.kube/config
+
+sleep 10
 
 kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-
-kubectl taint nodes --all node-role.kubernetes.io/master-
 
 #kubectl run discovery --image=dc243cam/dceks --port=6901
 #kubectl expose deployment discovery --type=NodePort
