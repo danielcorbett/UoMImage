451d077334e09b8d525e24e76c7f6f537112ea45
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-17 16:40:28 +0100

    Modified tigervnc.sh v2

--- scripts/tigervnc.sh
+++ scripts/tigervnc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export DISPLAY=$HOSTNAME:0
-
 yum install novnc xorg-x11-xinit tigervnc-server xterm xfce4-panel xfwm4 -y
-/usr/bin/novnc_server --vnc localhost:5901 --listen 6901 &
 
-xinit -- /usr/bin/Xvnc :0 -auth /root/.Xauthority -depth 24 -desktop $HOSTNAME:0 -fp /usr/share/fonts/X11//misc,/usr/share/fonts/X11//Type1 -geometry 1280x1024 -pn -SecurityTypes=none -rfbport 5901 -rfbwait 30000 -NeverShared &   
+#export DISPLAY=$HOSTNAME:0
+#/usr/bin/novnc_server --vnc localhost:5901 --listen 6901 &
 
-/usr/bin/xfce4-panel &
+#xinit -- /usr/bin/Xvnc :0 -auth /root/.Xauthority -depth 24 -desktop $HOSTNAME:0 -fp /usr/share/fonts/X11//misc,/usr/share/fonts/X11//Type1 -geometry 1280x1024 -pn -SecurityTypes=none -rfbport 5901 -rfbwait 30000 -NeverShared &   
 
-/usr/bin/xfwm4 &
+#/usr/bin/xfce4-panel &
+
+#/usr/bin/xfwm4 &
