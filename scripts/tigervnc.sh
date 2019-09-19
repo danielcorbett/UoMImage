89a60eacb18df202710b376e3ab8ba5887e4f0d0
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-19 12:24:39 +0100

    Updated vnc scripts

--- scripts/tigervnc.sh
+++ scripts/tigervnc.sh
@@ -14,5 +14,7 @@ echo "/usr/bin/novnc_server --vnc localhost:5901 --listen 6901 &" >> /usr/local/
 echo "xinit -- /usr/bin/Xvnc :0 -auth /root/.Xauthority -depth 24 -desktop $HOSTNAME:0 -fp /usr/share/fonts/X11//misc,/usr/share/fonts/X11//Type1 -geometry 1280x1024 -pn -SecurityTypes=none -rfbport 5901 -rfbwait 30000 -NeverShared &" >> /usr/local/bin/tigervnc.sh
 echo "/usr/bin/xfce4-panel &" >> /usr/local/bin/tigervnc.sh
 echo "/usr/bin/xfwm4 &" >> /usr/local/bin/tigervnc.sh
+echo "/usr/bin/xterm" >> /usr/local/bin/tigervnc.sh
+echo "/bin/bash" >> /usr/local/bin/tigervnc.sh
 
 chmod 700 /usr/local/bin/tigervnc.sh
