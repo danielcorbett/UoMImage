8c0b59ad80671cdd2a9d89e2e45de52ac2dfc261
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-25 15:10:15 +0100

    Added path.sh to juan.sh script v7

--- scripts/salome_meca.sh
+++ scripts/salome_meca.sh
@@ -8,8 +8,8 @@ tar -xvzf Salome-Meca-2017.0.1-LGPL-1.tgz
 
 wget https://iweb.dl.sourceforge.net/project/ramonelinux/Rel_0.99/releases/x86_64/packages/gcc-libstdc++-4.9.1-1.ram0.99.x86_64.rpm
 rpm2cpio gcc-libstdc++-4.9.1-1.ram0.99.x86_64.rpm | cpio -idmv
-mv usr/lib64/libstdc++.so.6 /usr/local/lib64/.
-ln -s /usr/local/lib64/libstdc++.so.6 /root/salome_meca/V2017.0.2/prerequisites/debianForSalome/lib
+mv usr/lib64/libstdc++.so.6.0.20 /usr/local/lib64/.
 
-./Salome-Meca-2017.0.1-LGPL-1.run
+./Salome-Meca-2017.0.1-LGPL-1.run -t /root/salome_meca -l English
+ln -s /usr/local/lib64/libstdc++.so.6.0.20 /root/salome_meca/V2017/prerequisites/debianForSalome/lib/libstd++.so.6
 
