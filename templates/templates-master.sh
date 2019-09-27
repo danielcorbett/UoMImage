d5b64211606a77706a84064fc18196cd790188cc
timfur15 <timothy.furmston@manchester.ac.uk>
2019-09-27 12:42:32 +0100

    Added templates creation script and static other files

---
+++ templates/templates-master.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+OUTFILE="./templates/other/template-selection.json"
+
+echo -e "\nPlease select which image to create...\n"
+echo -e "\n1) Juan (Code Aster & Code Saturn)"
+echo -e "\n2) MAD (Mongo DB, Apache Spark and Django)"
+echo -e "\n3) R"
+echo -e "\n4) VNC (TigerVNC)"
+echo -e ""
+read -p "Selection: " selection
+
+case "$selection" in
+	1)
+		BOX_NAME="juan"
+		PACKAGES="base-nogui.sh anaconda.sh vagrant.sh juan.sh cleanup.sh"
+		;;
+	2)
+		BOX_NAME="mad"
+		PACKAGES="base-nogui.sh anaconda.sh vagrant.sh MAD.sh cleanup.sh"
+		;;
+	3)
+		BOX_NAME="r"
+		PACKAGES="base-nogui.sh anaconda.sh vagrant.sh cleanup.sh R.sh"
+		;;
+	4)
+		BOX_NAME="vnc"
+		PACKAGES="base-nogui.sh anaconda.sh vagrant.sh tigervnc.sh"
+		;;
+	esac
+
+cat ./templates/other/template-top.json > $OUTFILE
+SIZE=0; for i in $PACKAGES; do SIZE=$((SIZE+1)); done
+COUNT=0
+for i in $PACKAGES
+do
+	COUNT=$((COUNT+1))
+	if [ ! $COUNT -eq $SIZE ]
+	then 
+		echo "            \"scripts\/$i\"," >> $OUTFILE
+	else
+		echo "            \"scripts\/$i\"" >> $OUTFILE
+	fi
+done
+cat ./templates/other/template-middle.json >> $OUTFILE
+echo -e "          \"output\": \"centos7-$BOX_NAME.box\"" >> $OUTFILE
+cat ./templates/other/template-bottom.json >> $OUTFILE
