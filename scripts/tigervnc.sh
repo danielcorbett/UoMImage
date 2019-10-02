fbc7138ded84867794ea70d79d80edfbee464ece
timfur15 <timothy.furmston@manchester.ac.uk>
2019-10-02 14:15:10 +0100

    Changes to tiger-vnc.sh to use Bash file descriptor and minor change to executable permissions

--- scripts/tigervnc.sh
+++ scripts/tigervnc.sh
@@ -9,13 +9,18 @@ yum install xterm -y
 yum install xfce4-panel -y
 yum install xfwm4 -y
 
-echo "export DISPLAY=$HOSTNAME:0" > /usr/local/bin/tigervnc.sh
-echo "export XFCE_PANEL_MIGRATE_DEFAULT=1" >> /usr/local/bin/tigervnc.sh
-echo "/usr/bin/novnc_server --vnc localhost:5901 --listen 6901 &" >> /usr/local/bin/tigervnc.sh
-echo "xinit -- /usr/bin/Xvnc :0 -auth /root/.Xauthority -depth 24 -desktop $HOSTNAME:0 -fp /usr/share/fonts/X11//misc,/usr/share/fonts/X11//Type1 -geometry 1280x1024 -pn -SecurityTypes=none -rfbport 5901 -rfbwait 30000 -NeverShared &" >> /usr/local/bin/tigervnc.sh
-echo "/usr/bin/xfce4-panel &" >> /usr/local/bin/tigervnc.sh
-echo "/usr/bin/xfwm4 &" >> /usr/local/bin/tigervnc.sh
-echo "/usr/bin/xterm" >> /usr/local/bin/tigervnc.sh
-echo "/bin/bash" >> /usr/local/bin/tigervnc.sh
+rm -f /usr/local/bin/tigervnc.sh
+exec 3<> /usr/local/bin/tigervnc.sh
 
-chmod 700 /usr/local/bin/tigervnc.sh
+echo "export DISPLAY=$HOSTNAME:0" >&3
+echo "export XFCE_PANEL_MIGRATE_DEFAULT=1" >&3
+echo "/usr/bin/novnc_server --vnc localhost:5901 --listen 6901 &" >&3
+echo "xinit -- /usr/bin/Xvnc :0 -auth /root/.Xauthority -depth 24 -desktop $HOSTNAME:0 -fp /usr/share/fonts/X11//misc,/usr/share/fonts/X11//Type1 -geometry 1280x1024 -pn -SecurityTypes=none -rfbport 5901 -rfbwait 30000 -NeverShared &" >&3
+echo "/usr/bin/xfce4-panel &" >&3
+echo "/usr/bin/xfwm4 &" >&3
+echo "/usr/bin/xterm" >&3
+echo "/bin/bash" >&3
+
+exec 3>&-
+
+chmod 755 /usr/local/bin/tigervnc.sh
